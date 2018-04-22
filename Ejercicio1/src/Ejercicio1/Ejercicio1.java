ecdee2851c8f9ed7d60cd328ceb5ece7e5838321
Lgsalgado <38463157+Lgsalgado@users.noreply.github.com>
2018-04-22 16:17:10 -0500

    Add files via upload

---
+++ Ejercicio1/src/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,13 @@
+package Ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args){
+        double fahrenheit=13.0;
+        double celsius =(fahrenheit -32.0)*(5.0/9.0);
+
+        System.out.println("Sus grados fahrenheit son: "+ fahrenheit);
+        System.out.println("Transformando a celsius es: "+celsius);
+
+
+    }
+}
